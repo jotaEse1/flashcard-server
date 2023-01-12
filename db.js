98cfd0597763a6f3b0e745b0586902f8aa2420c2
Joaquin <joacosanchez20@gmail.com>
2023-01-12 08:22:21 -0300

    create pool

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mysql = require('mysql');
 const util = require('util');
 require('dotenv').config()
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
+    connectionLimit: 10,
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
